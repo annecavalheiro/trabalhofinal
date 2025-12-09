9f210bcc5a917104481ffc0e35477f7d829121a6
Anne Oliveira <anneoliveira9080@gmail.com>
2025-12-09 05:33:57 -0300

    att

--- turista.h
+++ turista.h
@@ -10,9 +10,11 @@ const char *obterNomeTipoDeLugar(TipoDeLugar tipo);
 TipoDeLugar obterEnumTipoDeLugar(int opcao);
 void adicionarComentario();
 void listarComentarios();
+void listarEventosDaSemana();
 
 void visualizarLocal(int indiceLocal);
 void adicionarComentarioNoLocal(Local *l);
 void verTodosComentarios(Local *l);
+
 
 #endif
